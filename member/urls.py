f3f1988390f0f594be920e2ccccb0c114f95e686
jaehyuk <jaehyuk.pyon@gmail.com>
2023-01-26 12:44:59 +0900

    비밀번호 변경 구현

--- member/urls.py
+++ member/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.MemberRegisterView.as_view()),
     path('move/', views.login_page_move),    
     path('duplicate/', views.MemberIdDuplicateCheck.as_view()),
+    path('password/', views.MemberChangePasswordView.as_view()),
 ]
--- member/views.py
+++ member/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from .serializers import MemberRegisterSerializer
 from .models import Member
 from django.http import JsonResponse
+from django.contrib.auth.hashers import check_password, make_password
 
 # Create your views here.
 
@@ -31,3 +32,36 @@ class MemberIdDuplicateCheck(APIView):
         else:
             response_data['result'] = False
         return JsonResponse(response_data)
+    
+class MemberChangePasswordView(APIView):
+    
+    def put(self, request, *args, **kwargs):
+        
+        username = request.data.get('username')
+        current = request.data.get('old_password')
+        password1 = request.data.get('new_password1')
+        password2 = request.data.get('new_password2')
+        
+        if password1 != password2:
+            return Response({
+                'detail': 'New password does not match.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not Member.objects.filter(username=username).exists():
+            return Response({
+                'detail': 'No Account'
+            }, status=status.HTTP_404_NOT_FOUND)
+            
+        member = Member.objects.get(username=username)
+        
+        if not check_password(current, member.password):
+            return Response({
+                'detail': 'Old and New password do not match'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        member.password = make_password(password1)
+        member.save()
+        
+        return Response({
+            'detail': 'Successfully changed pw.'
+        }, status=status.HTTP_201_CREATED)
+        
